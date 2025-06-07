1f653da8e9594aa6c64bdb398178e3a8f36a9c5e
Deyand03 <defryyandy7@gmail.com>
2025-06-07 19:58:54 +0700

    menambahkan DBConfig, FormLogin

---
+++ manajemenDataMahasiswa/DBConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manajemenDataMahasiswa
+{
+    internal class DBConfig
+    {
+        public static string dbServer = "localhost";
+        public static string dbUser = "root";
+        public static string dbName = "zenith";
+        public static string dbPass = "";
+
+        public static string ConnStr => $"server={dbServer};user={dbUser};database={dbName};password={dbPass};";
+    }
+}
